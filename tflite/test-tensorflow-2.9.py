f26d0d0b7ad2ffa8e1d02d78b4036daf43ea94aa
Ed Vidal Jr <develone@sbcglobal.net>
2022-10-05 09:52:14 -0600

    update extra code.

--- tflite/test-tensorflow-2.9.py
+++ tflite/test-tensorflow-2.9.py
@@ -41,13 +41,138 @@ TEST_SPLIT = int(0.2 * SAMPLES + TRAIN_SPLIT)
 x_train, x_test, x_validate = np.split(x_values, [TRAIN_SPLIT, TEST_SPLIT])
 y_train, y_test, y_validate = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
 
+# We'll use 60% of our data for training and 20% for testing. The remaining 20%
+# will be used for validation. Calculate the indices of each section.
+TRAIN_SPLIT =  int(0.6 * SAMPLES)
+TEST_SPLIT = int(0.2 * SAMPLES + TRAIN_SPLIT)
+
+# Use np.split to chop our data into three parts.
+# The second argument to np.split is an array of indices where the data will be
+# split. We provide two indices, so the data will be divided into three chunks.
+x_train, x_test, x_validate = np.split(x_values, [TRAIN_SPLIT, TEST_SPLIT])
+y_train, y_test, y_validate = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
+
+# Double check that our splits add up correctly
 assert (x_train.size + x_validate.size + x_test.size) ==  SAMPLES
+
+# Plot the data in each partition in different colors:
 plt.plot(x_train, y_train, 'b.', label="Train")
 plt.plot(x_test, y_test, 'r.', label="Test")
 plt.plot(x_validate, y_validate, 'y.', label="Validate")
 plt.legend()
 plt.show()
 
+# We'll use 60% of our data for training and 20% for testing. The remaining 20%
+# will be used for validation. Calculate the indices of each section.
+TRAIN_SPLIT =  int(0.6 * SAMPLES)
+TEST_SPLIT = int(0.2 * SAMPLES + TRAIN_SPLIT)
+
+# Use np.split to chop our data into three parts.
+# The second argument to np.split is an array of indices where the data will be
+# split. We provide two indices, so the data will be divided into three chunks.
+x_train, x_test, x_validate = np.split(x_values, [TRAIN_SPLIT, TEST_SPLIT])
+y_train, y_test, y_validate = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
+
+# Double check that our splits add up correctly
+assert (x_train.size + x_validate.size + x_test.size) ==  SAMPLES
+
+# Plot the data in each partition in different colors:
+plt.plot(x_train, y_train, 'b.', label="Train")
+plt.plot(x_test, y_test, 'r.', label="Test")
+plt.plot(x_validate, y_validate, 'y.', label="Validate")
+plt.legend()
+plt.show()
+# We'll use 60% of our data for training and 20% for testing. The remaining 20%
+# will be used for validation. Calculate the indices of each section.
+TRAIN_SPLIT =  int(0.6 * SAMPLES)
+TEST_SPLIT = int(0.2 * SAMPLES + TRAIN_SPLIT)
+
+# Use np.split to chop our data into three parts.
+# The second argument to np.split is an array of indices where the data will be
+# split. We provide two indices, so the data will be divided into three chunks.
+x_train, x_test, x_validate = np.split(x_values, [TRAIN_SPLIT, TEST_SPLIT])
+y_train, y_test, y_validate = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
+
+# Double check that our splits add up correctly
+assert (x_train.size + x_validate.size + x_test.size) ==  SAMPLES
+
+# Plot the data in each partition in different colors:
+plt.plot(x_train, y_train, 'b.', label="Train")
+plt.plot(x_test, y_test, 'r.', label="Test")
+plt.plot(x_validate, y_validate, 'y.', label="Validate")
+plt.legend()
+plt.show()
+# We'll use 60% of our data for training and 20% for testing. The remaining 20%
+# will be used for validation. Calculate the indices of each section.
+TRAIN_SPLIT =  int(0.6 * SAMPLES)
+TEST_SPLIT = int(0.2 * SAMPLES + TRAIN_SPLIT)
+
+# Use np.split to chop our data into three parts.
+# The second argument to np.split is an array of indices where the data will be
+# split. We provide two indices, so the data will be divided into three chunks.
+x_train, x_test, x_validate = np.split(x_values, [TRAIN_SPLIT, TEST_SPLIT])
+y_train, y_test, y_validate = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
+
+# Double check that our splits add up correctly
+assert (x_train.size + x_validate.size + x_test.size) ==  SAMPLES
+
+# Plot the data in each partition in different colors:
+plt.plot(x_train, y_train, 'b.', label="Train")
+plt.plot(x_test, y_test, 'r.', label="Test")
+plt.plot(x_validate, y_validate, 'y.', label="Validate")
+plt.legend()
+plt.show()
+# We'll use 60% of our data for training and 20% for testing. The remaining 20%
+# will be used for validation. Calculate the indices of each section.
+TRAIN_SPLIT =  int(0.6 * SAMPLES)
+TEST_SPLIT = int(0.2 * SAMPLES + TRAIN_SPLIT)
+
+# Use np.split to chop our data into three parts.
+# The second argument to np.split is an array of indices where the data will be
+# split. We provide two indices, so the data will be divided into three chunks.
+x_train, x_test, x_validate = np.split(x_values, [TRAIN_SPLIT, TEST_SPLIT])
+y_train, y_test, y_validate = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
+
+# Double check that our splits add up correctly
+assert (x_train.size + x_validate.size + x_test.size) ==  SAMPLES
+
+# Plot the data in each partition in different colors:
+plt.plot(x_train, y_train, 'b.', label="Train")
+plt.plot(x_test, y_test, 'r.', label="Test")
+plt.plot(x_validate, y_validate, 'y.', label="Validate")
+plt.legend()
+plt.show()
+
+plt.plot(x_train, y_train, 'b.', label="Train")
+plt.plot(x_test, y_test, 'r.', label="Test")
+plt.plot(x_validate, y_validate, 'y.', label="Validate")
+plt.legend()
+plt.show()
+
+# We'll use Keras to create a simple model architecture
+model_1 = tf.keras.Sequential()
+
+# First layer takes a scalar input and feeds it through 8 "neurons". The
+# neurons decide whether to activate based on the 'relu' activation function.
+model_1.add(keras.layers.Dense(8, activation='relu', input_shape=(1,)))
+
+# Final layer is a single neuron, since we want to output a single value
+model_1.add(keras.layers.Dense(1))
+
+# We'll use Keras to create a simple model architecture
+model_1 = tf.keras.Sequential()
+
+# First layer takes a scalar input and feeds it through 8 "neurons". The
+# neurons decide whether to activate based on the 'relu' activation function.
+model_1.add(keras.layers.Dense(8, activation='relu', input_shape=(1,)))
+
+# Final layer is a single neuron, since we want to output a single value
+model_1.add(keras.layers.Dense(1))
+
+# Compile the model using the standard 'adam' optimizer and the mean squared error or 'mse' loss function for regression.
+model_1.compile(optimizer='adam', loss='mse', metrics=['mae'])
+# Compile the model using the standard 'adam' optimizer and the mean squared error or 'mse' loss function for regression.
+model_1.compile(optimizer='adam', loss='mse', metrics=['mae'])
 model_1 = tf.keras.Sequential()
 
 model_1.add(keras.layers.Dense(8, activation='relu', input_shape=(1,)))
@@ -163,6 +288,77 @@ plt.legend()
 
 plt.tight_layout()
 
+# Calculate and print the loss on our test dataset
+test_loss, test_mae = model.evaluate(x_test, y_test)
+
+# Make predictions based on our test dataset
+y_test_pred = model.predict(x_test)
+
+# Graph the predictions against the actual values
+plt.clf()
+plt.title('Comparison of predictions and actual values')
+plt.plot(x_test, y_test, 'b.', label='Actual values')
+plt.plot(x_test, y_test_pred, 'r.', label='TF predicted')
+plt.legend()
+plt.show()
+
+model = tf.keras.Sequential()
+
+# First layer takes a scalar input and feeds it through 16 "neurons". The
+# neurons decide whether to activate based on the 'relu' activation function.
+model.add(keras.layers.Dense(16, activation='relu', input_shape=(1,)))
+
+# The new second and third layer will help the network learn more complex representations
+model.add(keras.layers.Dense(16, activation='relu'))
+
+# Final layer is a single neuron, since we want to output a single value
+model.add(keras.layers.Dense(1))
+
+# Compile the model using the standard 'adam' optimizer and the mean squared error or 'mse' loss function for regression.
+model.compile(optimizer='adam', loss="mse", metrics=["mae"])
+
+# Train the model
+history = model.fit(x_train, y_train, epochs=500, batch_size=64,
+                    validation_data=(x_validate, y_validate))
+
+# Save the model to disk
+model.save(MODEL_TF)
+
+# Draw a graph of the loss, which is the distance between
+# the predicted and actual values during training and validation.
+train_loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(train_loss) + 1)
+
+# Exclude the first few epochs so the graph is easier to read
+SKIP = 100
+
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+
+plt.plot(epochs[SKIP:], train_loss[SKIP:], 'g.', label='Training loss')
+plt.plot(epochs[SKIP:], val_loss[SKIP:], 'b.', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+
+# Draw a graph of mean absolute error, which is another way of
+# measuring the amount of error in the prediction.
+train_mae = history.history['mae']
+val_mae = history.history['val_mae']
+
+plt.plot(epochs[SKIP:], train_mae[SKIP:], 'g.', label='Training MAE')
+plt.plot(epochs[SKIP:], val_mae[SKIP:], 'b.', label='Validation MAE')
+plt.title('Training and validation mean absolute error')
+plt.xlabel('Epochs')
+plt.ylabel('MAE')
+plt.legend()
+
+plt.tight_layout()
 # Calculate and print the loss on our test dataset
 test_loss, test_mae = model.evaluate(x_test, y_test)
 
