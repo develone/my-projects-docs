207cd9df12a23ddd9a7b2affd148a4da2f6193fc
Ed Vidal Jr <develone@sbcglobal.net>
2022-10-04 20:49:59 -0600

    adding more code

--- tflite/test-tensorflow-2.9.py
+++ tflite/test-tensorflow-2.9.py
@@ -1,3 +1,13 @@
+# Define paths to model files
+import os
+MODELS_DIR = 'models/'
+if not os.path.exists(MODELS_DIR):
+    os.mkdir(MODELS_DIR)
+MODEL_TF = MODELS_DIR + 'model'
+MODEL_NO_QUANT_TFLITE = MODELS_DIR + 'model_no_quant.tflite'
+MODEL_TFLITE = MODELS_DIR + 'model.tflite'
+MODEL_TFLITE_MICRO = MODELS_DIR + 'model.cc'
+
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -81,4 +91,88 @@ plt.ylabel('MAE')
 plt.legend()
 plt.show()
 
+# Calculate and print the loss on our test dataset
+test_loss, test_mae = model_1.evaluate(x_test, y_test)
 
+# Make predictions based on our test dataset
+y_test_pred = model_1.predict(x_test)
+
+# Graph the predictions against the actual values
+plt.clf()
+plt.title('Comparison of predictions and actual values')
+plt.plot(x_test, y_test, 'b.', label='Actual values')
+plt.plot(x_test, y_test_pred, 'r.', label='TF predictions')
+plt.legend()
+plt.show()
+
+model = tf.keras.Sequential()
+
+# First layer takes a scalar input and feeds it through 16 "neurons". The
+# neurons decide whether to activate based on the 'relu' activation function.
+model.add(keras.layers.Dense(16, activation='relu', input_shape=(1,)))
+
+# The new second and third layer will help the network learn more complex representations
+model.add(keras.layers.Dense(16, activation='relu'))
+
+# Final layer is a single neuron, since we want to output a single value
+model.add(keras.layers.Dense(1))
+
+# Compile the model using the standard 'adam' optimizer and the mean squared error or 'mse' loss function for regression.
+model.compile(optimizer='adam', loss="mse", metrics=["mae"])
+
+# Train the model
+history = model.fit(x_train, y_train, epochs=500, batch_size=64,
+                    validation_data=(x_validate, y_validate))
+
+# Save the model to disk
+model.save(MODEL_TF)
+
+# Draw a graph of the loss, which is the distance between
+# the predicted and actual values during training and validation.
+train_loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(train_loss) + 1)
+
+# Exclude the first few epochs so the graph is easier to read
+SKIP = 100
+
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+
+plt.plot(epochs[SKIP:], train_loss[SKIP:], 'g.', label='Training loss')
+plt.plot(epochs[SKIP:], val_loss[SKIP:], 'b.', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+
+# Draw a graph of mean absolute error, which is another way of
+# measuring the amount of error in the prediction.
+train_mae = history.history['mae']
+val_mae = history.history['val_mae']
+
+plt.plot(epochs[SKIP:], train_mae[SKIP:], 'g.', label='Training MAE')
+plt.plot(epochs[SKIP:], val_mae[SKIP:], 'b.', label='Validation MAE')
+plt.title('Training and validation mean absolute error')
+plt.xlabel('Epochs')
+plt.ylabel('MAE')
+plt.legend()
+
+plt.tight_layout()
+
+# Calculate and print the loss on our test dataset
+test_loss, test_mae = model.evaluate(x_test, y_test)
+
+# Make predictions based on our test dataset
+y_test_pred = model.predict(x_test)
+
+# Graph the predictions against the actual values
+plt.clf()
+plt.title('Comparison of predictions and actual values')
+plt.plot(x_test, y_test, 'b.', label='Actual values')
+plt.plot(x_test, y_test_pred, 'r.', label='TF predicted')
+plt.legend()
+plt.show()
