c72533747a3094934b6fb5397dde39871109e7bb
Ed Vidal Jr <develone@sbcglobal.net>
2022-10-10 09:22:33 -0600

    used to add pkgs to pi4-27 32 bit and pi4-34 64 bit

---
+++ pkg-install-scripts/extra_pkgs_64bit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install octave  vlc kicad  hexchat  \
+ gawk gimp gtkwave iverilog clang \
+samba samba-common-bin smbclient cifs-utils \
+libftdi-dev libmpc-dev libmpfr-dev squashfs-tools bc mercurial \
+libbison-dev libgmp-dev libelf-dev ncurses-dev \
+default-jdk libreadline-dev xdot graphviz minicom xpdf lrzsz \
+libusb-1.0.0 exuberant-ctags gparted ffmpeg \
+mesa-utils libboost-all-dev freeglut3 freeglut3-dev \
+python3-dev libgtk2.0-dev libcairo2-dev libpango1.0-dev \
+libgdk-pixbuf2.0-dev libatk1.0-dev libghc-x11-dev binutils-arm-none-eabi  \
+gcc-arm-none-eabi diffuse gitk tcl-dev telnet tftp hexedit cmake flex bison \
+gtkmm-3.0 gperf build-essential nfs-kernel-server gpac \
+libeigen3-dev libffi-dev libssl-dev python python3-pip libtiff-dev ppp \
+gdb-multiarch firefox-esr ninja-build code ecryptfs-utils \
+liboctave-dev rpi-imager autoconf automake libreoffice indent iperf \
+wireshark
+#sudo apt-get remove python-configparser
+
+#sudo pip install docker-compose
