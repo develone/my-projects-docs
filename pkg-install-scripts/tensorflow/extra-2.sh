5ff95372e759d14d942032223e535dbd6ac01e16
Ed Vidal Jr <develone@sbcglobal.net>
2022-10-10 09:27:32 -0600

    install extra depend for tensorflow pi4-34 64 bit

---
+++ pkg-install-scripts/tensorflow/extra-2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install -y libhdf5-dev libc-ares-dev libeigen3-dev gcc gfortran libgfortran5 libatlas3-base libatlas-base-dev libopenblas-dev libopenblas-base libblas-dev liblapack-dev cython3 libatlas-base-dev openmpi-bin libopenmpi-dev python3-dev
---
+++ pkg-install-scripts/tensorflow/extra-3.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install --yes libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libgdbm-dev lzma lzma-dev tcl-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev wget curl make build-essential openssl
