3f9226b6bf5ff1ad5aaa1bd5d84a19da068524f6
Ed Vidal Jr <develone@sbcglobal.net>
2022-10-27 08:16:20 -0600

    needed to learn about ip6 networks

---
+++ pico_w-wifi-test/access-point/ip6test.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+ping -c5 -I wlan0 fe80::7db3:42b9:89b8:41e4
+
+ping -c5 -I wlan0 fe80::4517:f78f:11d7:4206
+
+ping -c5 -I wlan0 fe80::b972:93a:e9cb:e471
+
+ping -c5 -I wlan0 fe80::512e:9914:dcf3:d8e2
+
+ping -c5 -I wlan0 fe80::9017:d6a9:a6c:d65c
+
+ping -c5 -I wlan0 fe80::8c32:60c1:78da:baf5
