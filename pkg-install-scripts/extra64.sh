f07806671e7305efe0b6ace2befde0bfe03fc716
Ed Vidal Jr <develone@sbcglobal.net>
2023-02-18 10:28:42 -0700

    latest pkgs update

---
+++ pkg-install-scripts/extra64.sh
@@ -0,0 +1,39 @@
+
+
+
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install octave  vlc kicad  hexchat  \
+ gawk gimp gtkwave iverilog clang \
+samba samba-common-bin smbclient cifs-utils \
+libftdi-dev libmpc-dev libmpfr-dev squashfs-tools bc mercurial \
+libbison-dev libgmp-dev libelf-dev ncurses-dev \
+default-jdk libreadline-dev xdot graphviz minicom xpdf lrzsz \
+libusb-1.0.0 exuberant-ctags gparted ffmpeg \
+mesa-utils libboost-all-dev freeglut3 freeglut3-dev \
+python3-dev libgtk2.0-dev libcairo2-dev libpango1.0-dev \
+libgdk-pixbuf2.0-dev libatk1.0-dev libghc-x11-dev binutils-arm-none-eabi  \
+gcc-arm-none-eabi diffuse gitk tcl-dev telnet tftp hexedit cmake flex bison \
+gtkmm-3.0 gperf build-essential nfs-kernel-server gpac \
+libeigen3-dev libffi-dev libssl-dev python python3-pip libtiff-dev ppp \
+gdb-multiarch firefox-esr ninja-build code ecryptfs-utils \
+liboctave-dev rpi-imager autoconf automake libreoffice indent iperf \
+wireshark nmap  dnsmasq mosquitto mosquitto-clients fritzing doxygen espeak \
+libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm \
+libncurses5-dev libncursesw5-dev xz-utils tk-dev libgdbm-dev lzma \
+lzma-dev tcl-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+wget curl make build-essential openssl libhdf5-dev libc-ares-dev \
+libeigen3-dev gcc gfortran libgfortran5 libatlas3-base libatlas-base-dev \
+libopenblas-dev libopenblas-base libblas-dev liblapack-dev cython3 \
+libatlas-base-dev openmpi-bin libopenmpi-dev python3-dev \
+libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev \
+llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libgdbm-dev \
+lzma lzma-dev tcl-dev libxml2-dev libxmlsec1-dev libffi-dev \
+liblzma-dev wget curl make build-essential openssl \
+libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev \
+llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libgdbm-dev \
+lzma lzma-dev tcl-dev libxml2-dev libxmlsec1-dev libffi-dev \
+liblzma-dev wget curl make build-essential openssl \
+libusb-dev libreadline-dev libglib2.0-dev libudev-dev libdbus-1-dev \
+libical-dev libmosquitto-dev
+
